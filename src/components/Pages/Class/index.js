081b88a1aefc15eade03bc5ff15792d5a2ee5dd5
ninh.uit@gmail.com <ninh.uit@gmail.com>
2018-02-11 23:08:06 +0700

    edit eslint, update author

--- src/components/Pages/Class/index.js
+++ src/components/Pages/Class/index.js
@@ -1,10 +1,10 @@
-/* --------------------------------------------------------
-* Author Trần Đức Tiến
-* Email ductienas@gmail.com
-* Phone 0972970075
-*
-* Created: 2018-02-08 10:26:56
-*------------------------------------------------------- */
+/*--------------------------------------------------------
+ * Author Ngo An Ninh
+ * Email ninh.uit@gmail.com
+ * Phone 0978 108 807
+ *
+ * Created: 2018-01-10 23:20:59
+ *-------------------------------------------------------*/
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
--- src/redux/reducers/class.js
+++ src/redux/reducers/class.js
@@ -10,37 +10,37 @@ import Router from 'next/router';
 export const initialState = {
 	classList: [],
 	classInfo: {
-        name: '',
-        desc: '',
-        status: 'active'
-    }
+		name: '',
+		desc: '',
+		status: 'active',
+	},
 };
 
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case 'CLASS_VALUE_CHANGE':
 			return {
-                ...state,
-                classInfo: {
-                    ...state.classInfo,
-                    [action.payload.key]: action.payload.value
-                }
-            }
-        case 'GET_CLASSES_SUCCESS':
-            return {
-                ...state,
-                classList: action.payload.data
-            }
-        case 'GET_CLASS_SUCCESS':
-            return {
-                ...state,
-                classInfo: action.payload
-            }
-        case 'CLASS_RESET_STATE_INFO':
-            return {
-                ...state,
-                classInfo: initialState.classInfo
-            }
+				...state,
+				classInfo: {
+					...state.classInfo,
+					[action.payload.key]: action.payload.value,
+				},
+			};
+		case 'GET_CLASSES_SUCCESS':
+			return {
+				...state,
+				classList: action.payload.data,
+			};
+		case 'GET_CLASS_SUCCESS':
+			return {
+				...state,
+				classInfo: action.payload,
+			};
+		case 'CLASS_RESET_STATE_INFO':
+			return {
+				...state,
+                classInfo: initialState.classInfo,
+			};
 		default:
 			return state;
 	}
